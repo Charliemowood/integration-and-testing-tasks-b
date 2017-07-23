427ac186daf6f919c3e803a72ff6ee33f96b4233
Charlie Wood <charliemowood@gmail.com>
2017-07-23 15:40:56 +0100

    Can add 10

--- js_calculator/tests/unit/calculator_spec.js
+++ js_calculator/tests/unit/calculator_spec.js
@@ -11,4 +11,37 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('it can add 10', function(){
+    calculator.add(10)
+    assert.equal(10, calculator.runningTotal)
+  })
+
+  xit('it can subtract', function(){
+    assert.equal(true, true)
+  })
+
+  xit('it can multiply', function(){
+    assert.equal(true, true)
+  })
+
+  xit('it can devide', function(){
+    assert.equal(true, true)
+  })
+
+  xit('it can number click', function(){
+    assert.equal(true, true)
+  })
+
+  xit('it can click operator', function(){
+    assert.equal(true, true)
+  })
+
+  xit('it can clear', function(){
+    assert.equal(true, true)
+  })
+
+
+
+
+
 });
