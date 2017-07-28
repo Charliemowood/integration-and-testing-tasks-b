b2646541d1da7df7d82a46a1b8b7ef9e2cf9f4a9
Charlie Wood <charliemowood@gmail.com>
2017-07-28 09:46:50 +0100

    Add test to check that decimals work

--- js_calculator/tests/integration/tests.js
+++ js_calculator/tests/integration/tests.js
@@ -103,5 +103,16 @@ describe('calculator functionality', function() {
     })
 
 
+    it('should work with decimals', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number7')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('3.5')
+    })
+
+
+
 
 });
