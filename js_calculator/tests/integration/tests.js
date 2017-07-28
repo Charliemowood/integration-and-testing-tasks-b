c64469305e87095e7b50e369af665d42fa13dea5
Charlie Wood <charliemowood@gmail.com>
2017-07-28 09:24:32 +0100

    Add first integration tests

--- js_calculator/tests/integration/tests.js
+++ js_calculator/tests/integration/tests.js
@@ -16,4 +16,19 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should have working number buttons, testing 3', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
+  it('should have working number buttons, testing muliple buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('33')
+  })
+
+
+
 });
