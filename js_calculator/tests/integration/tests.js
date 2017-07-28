e533572948d031111a742aefbd76454578be8fbb
Charlie Wood <charliemowood@gmail.com>
2017-07-28 09:34:45 +0100

    Add test concerning chaining multiple operators

--- js_calculator/tests/integration/tests.js
+++ js_calculator/tests/integration/tests.js
@@ -66,5 +66,21 @@ describe('calculator functionality', function() {
   })
 
 
+    it('should be able to chain multiple operators', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number5')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number3')).click();
+      element(by.css('#operator_add')).click();
+      element(by.css('#number4')).click();
+      element(by.css('#operator_divide')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_multiply')).click();
+      element(by.css('#number2')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('6')
+    })
+
+
 
 });
