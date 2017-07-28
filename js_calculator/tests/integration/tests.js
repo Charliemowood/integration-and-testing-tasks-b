3900f1455cd8e76852edd286fc21b2c218b555ff
Charlie Wood <charliemowood@gmail.com>
2017-07-28 09:40:19 +0100

    calculator functionality should work with big positive and negative numbers

--- js_calculator/tests/integration/tests.js
+++ js_calculator/tests/integration/tests.js
@@ -81,6 +81,27 @@ describe('calculator functionality', function() {
       expect(running_total.getAttribute('value')).to.eventually.equal('6')
     })
 
+    it('should work with big positive and negative numbers', function(){
+      running_total = element(by.css('#running_total'))
+      element(by.css('#number6')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_subtract')).click();
+      element(by.css('#number6')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number5')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#number0')).click();
+      element(by.css('#operator_equals')).click();
+      expect(running_total.getAttribute('value')).to.eventually.equal('-5000')
+    })
+
 
 
 });
